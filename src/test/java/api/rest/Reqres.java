26e2b57a9efb9fff17cfa0fc92f8218878b25b2c
vadim1504 <vadi-96@mail.ri>
2017-10-02 16:39:54 +0300

    several pages

--- src/test/java/api/rest/Reqres.java
+++ src/test/java/api/rest/Reqres.java
@@ -27,9 +27,17 @@ public class Reqres {
 
     @Test()
     public void getUsers(){
-        get("/users").then().statusCode(200);
-        String response = get("/users").asString();
-        Integer id = JsonPath.from(response).get("data.find { it.first_name == 'Emma' }.id");
-        given().when().get("users/"+id).then().statusCode(200).body("data.first_name",equalTo("Emma")).body("data.last_name",equalTo("Wong")).body("data.avatar",equalTo("https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"));
+        String response = get("/users").then().statusCode(200).extract().asString();
+        int total_page = JsonPath.from(response).getInt("total_pages");
+        int id;
+        for (int i=1;i<=total_page;i++){
+            response = get("/users?page="+i).then().statusCode(200).extract().asString();
+            try {
+                id = JsonPath.from(response).getInt("data.find { it.first_name == 'Emma' }.id");
+                given().when().get("users/"+id).then().statusCode(200).body("data.first_name",equalTo("Emma")).body("data.last_name",equalTo("Wong")).body("data.avatar",equalTo("https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"));
+            }catch (NullPointerException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
